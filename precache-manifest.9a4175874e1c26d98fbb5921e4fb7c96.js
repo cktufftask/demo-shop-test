f7c0c2171fa1ce65a9c9df74ebd7fc79c7b829d9
cktufftask <61330012+cktufftask@users.noreply.github.com>
2020-02-22 02:21:30 +0530

    Updates

---
+++ precache-manifest.9a4175874e1c26d98fbb5921e4fb7c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cf96b225a5b4e5c90b1",
+    "url": "/static/js/main.be4eea11.chunk.js"
+  },
+  {
+    "revision": "20e24cdbf5880f146429",
+    "url": "/static/js/2.04ff8a47.chunk.js"
+  },
+  {
+    "revision": "7cf96b225a5b4e5c90b1",
+    "url": "/static/css/main.2b282203.chunk.css"
+  },
+  {
+    "revision": "20e24cdbf5880f146429",
+    "url": "/static/css/2.921641cb.chunk.css"
+  },
+  {
+    "revision": "ff1eec2c54d00b525b9657be66984b35",
+    "url": "/index.html"
+  }
+];
