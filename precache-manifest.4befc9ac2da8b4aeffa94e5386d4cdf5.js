d30dcb6c835043f3e128f33da77671ef79f6ee4c
cktufftask <61330012+cktufftask@users.noreply.github.com>
2020-02-22 01:55:05 +0530

    Updates

---
+++ precache-manifest.4befc9ac2da8b4aeffa94e5386d4cdf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75a695de0c5a7696d9c4",
+    "url": "/cktufftask/demo-shop-test/static/js/runtime~main.74c7e406.js"
+  },
+  {
+    "revision": "7cf96b225a5b4e5c90b1",
+    "url": "/cktufftask/demo-shop-test/static/js/main.be4eea11.chunk.js"
+  },
+  {
+    "revision": "20e24cdbf5880f146429",
+    "url": "/cktufftask/demo-shop-test/static/js/2.04ff8a47.chunk.js"
+  },
+  {
+    "revision": "7cf96b225a5b4e5c90b1",
+    "url": "/cktufftask/demo-shop-test/static/css/main.2b282203.chunk.css"
+  },
+  {
+    "revision": "20e24cdbf5880f146429",
+    "url": "/cktufftask/demo-shop-test/static/css/2.921641cb.chunk.css"
+  },
+  {
+    "revision": "ff7165c7ed64f397834cc0d6167b9456",
+    "url": "/cktufftask/demo-shop-test/index.html"
+  }
+];
